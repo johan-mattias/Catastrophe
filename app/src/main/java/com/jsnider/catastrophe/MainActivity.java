8c86d5aa191293632588f80140046ebd59f6bb8a
Johan Snider <johan.snider@gmail.com>
2019-07-30 00:54:50 +0200

    working gridview

--- app/src/main/java/com/jsnider/catastrophe/MainActivity.java
+++ app/src/main/java/com/jsnider/catastrophe/MainActivity.java
@@ -1,22 +1,18 @@
 package com.jsnider.catastrophe;
 
-import android.graphics.Bitmap;
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
@@ -27,33 +23,32 @@ import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView resultTextView;
     private EditText searchEditText;
     private final LinkedList<String> mWordList = new LinkedList<>();
     private final LinkedList<String> urlList = new LinkedList<>();
     private RecyclerView mRecyclerView;
     private ListAdapter mAdapter;
-
     RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        resultTextView = findViewById(R.id.resultTextView);
         searchEditText = findViewById(R.id.searchEditText);
         queue = Volley.newRequestQueue(this);
-
     }
 
     public void search(View view) {
+        mRecyclerView = findViewById(R.id.recyclerview);
+        mRecyclerView.removeAllViewsInLayout();
+
         String query = searchEditText.getText().toString();
-        resultTextView.setText(R.string.napping);
 
         String url ="https://www.flickr.com/services/rest/?" +
                 "method=flickr.photos.search&" +
                 "api_key=3e0b862c934cf2ae6c6e8f55268e44c5&" +
                 "tags=goat,goats," + query + "&" +
+                "tag_mode=all&" +
                 "format=json&" +
                 "nojsoncallback=1";
 
@@ -63,17 +58,12 @@ public class MainActivity extends AppCompatActivity {
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        // Display the first 500 characters of the response string.
-                        //resultTextView.setText("url" + "        Response is: "+ response.toString().substring(0,500));
-
                         JSONArray photoArray = null;
                         try {
                             photoArray = response.getJSONObject("photos").getJSONArray("photo");
                         } catch (JSONException e) {
                             Log.e("error", e.toString());
                         }
-
-//                        resultTextView.setText("url" + "Response is: "+ photoArray.toString().substring(0,500));
 
                         String item = null;
                         String url = null;
@@ -100,18 +90,18 @@ public class MainActivity extends AppCompatActivity {
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                resultTextView.setText("That didn't work!");
+                Log.e("ErrorListener", "Error on responce listener");
             }
         });
 
         // Add the request to the RequestQueue.
         queue.add(jsonObjectRequest);
 
-        mRecyclerView = findViewById(R.id.recyclerview);
         mAdapter = new ListAdapter(this, mWordList, urlList);
         mRecyclerView.setAdapter(mAdapter);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 3));
 
+        view.clearFocus();
     }
 
 }
