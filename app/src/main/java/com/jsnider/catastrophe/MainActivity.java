8f1b7dc3c0acfff8d4a05a149e89b9e24fd05478
Johan Snider <johan.snider@gmail.com>
2019-07-29 21:05:02 +0200

    changed from async task to volley, working API call to flikr

--- app/src/main/java/com/jsnider/catastrophe/MainActivity.java
+++ app/src/main/java/com/jsnider/catastrophe/MainActivity.java
@@ -8,6 +8,13 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import java.lang.ref.WeakReference;
 import java.util.Random;
 
@@ -16,50 +23,50 @@ public class MainActivity extends AppCompatActivity {
     private TextView resultTextView;
     private EditText searchEditText;
 
+    RequestQueue queue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         resultTextView = findViewById(R.id.resultTextView);
         searchEditText = findViewById(R.id.searchEditText);
+
+        queue = Volley.newRequestQueue(this);
     }
 
     public void search(View view) {
         String query = searchEditText.getText().toString();
         resultTextView.setText(R.string.napping);
-        new SearchAsyncTask(resultTextView).execute(query);
+        //new SearchAsyncTask(resultTextView).execute(query);
+
+        String url ="https://www.flickr.com/services/rest/?" +
+                "method=flickr.photos.search&" +
+                "api_key=3e0b862c934cf2ae6c6e8f55268e44c5&" +
+                "tags=cats&" +
+                "tag_mode=all&" +
+                "format=json&" +
+                "nojsoncallback=1";
+
+        //from: https://developer.android.com/training/volley/simple.html
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        resultTextView.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                resultTextView.setText("That didn't work!");
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
     }
-}
 
-class SearchAsyncTask
-        extends AsyncTask<String, Integer, String> {
-
-    private WeakReference<TextView> resultTextView;
-
-    SearchAsyncTask(TextView tv) {
-        resultTextView = new WeakReference<>(tv);
-    }
-
-    @Override
-    protected String doInBackground(String... strings) {
-
-        String s = strings[0];
-
-        Random r = new Random();
-        int n = r.nextInt(11);
-
-        int sleep = n * 200;
-
-        try {
-            Thread.sleep(sleep);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        return "We're searching for a " + s + " cat!";
-    }
-
-    protected void onPostExecute(String result) {
-        resultTextView.get().setText(result);
-    }
 }
